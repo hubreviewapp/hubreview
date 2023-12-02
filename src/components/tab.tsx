e913d50cf1f83138ffa3e9611a6e7ba1a84354a6
virem <viremaydin@gmail.com>
2023-12-02 17:51:44 +0300

    tab.tsx

---
+++ src/components/tab.tsx
@@ -0,0 +1,39 @@
+// src/TabComponent.tsx
+import React from 'react';
+import { Tabs, Tab, Paper } from '@mui/material';
+
+interface TabComponentProps {
+    tabs: string[];
+}
+
+const TabComponent: React.FC<TabComponentProps> = ({ tabs }) => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div style={{ padding: 16 }}>
+            <Paper elevation={3}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    centered
+                >
+                    {tabs.map((tab, index) => (
+                        <Tab key={index} label={tab} />
+                    ))}
+                </Tabs>
+            </Paper>
+            {/* Add your tab content here based on the selected tab value */}
+            {tabs.map((content, index) => (
+                value === index && <div key={index}>{content} Content</div>
+            ))}
+        </div>
+    );
+};
+
+export default TabComponent;
