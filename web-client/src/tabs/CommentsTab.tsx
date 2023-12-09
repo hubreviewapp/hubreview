115b880189c8aa29bad244598dcdc677f7eb0da5
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2023-12-09 23:23:44 +0300

    feat:Pr detail page additions

--- web-client/src/tabs/CommentsTab.tsx
+++ web-client/src/tabs/CommentsTab.tsx
@@ -4,15 +4,20 @@ function CommentsTab() {
   const comments = ["x.py", "y.py", "z.py"];
   return (
     <div>
-      {comments.map((comment) => (
-        <Comment key={comment}></Comment>
+      {comments.map((comment, index) => (
+        <div>
+          <Comment key={comment}
+                   id={index}
+                   author={"aysekelleci"}
+                   text={"In every project I'm using Zodios in, I'm eventually seeing more and more \"implicit any\" " +
+            "warnings which go away when restarting the TS language server in VS Code or sometimes even just saving my current file. " +
+            "Looks like TS gets confused as to what typings to pick or something like that (just like you suggested)."}
+                   date={new Date(2023, 4, 7)}>
+          </Comment>
+          <br></br>
+        </div>
       ))}
 
-      {comments.map((comment) => (
-        <h1 key={comment}>{comment}</h1>
-      ))}
-
-      <hr></hr>
     </div>
   );
 }
