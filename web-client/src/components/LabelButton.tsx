1897a7ea13b5794329ba18fd2dfd2f2582557ddc
virem <viremaydin@gmail.com>
2023-12-11 11:19:15 +0300

    feat: label icon

--- web-client/src/components/LabelButton.tsx
+++ web-client/src/components/LabelButton.tsx
@@ -1,4 +1,5 @@
-import {Badge, Box} from "@mantine/core";
+import {Badge, Box, rem} from "@mantine/core";
+import {IconTag} from "@tabler/icons-react";
 
 interface LabelButtonProps {
   label: string;
@@ -10,10 +11,12 @@ function LabelButton({ label, size }: LabelButtonProps) {
   const gradient: string[]  =
     label === "enhancement" ? ["pink","violet"] : label === "bug fix" ? ["red","pink"] :
       label == "refactoring" ? ["teal","lime"] : ["gray", "indigo"];
+  const iconTag = <IconTag style={{ width: rem(15), height: rem(15) }} />;
 
   return (
     <Box>
         <Badge
+          leftSection={iconTag}
           size= {size}
           variant="gradient"
           gradient={{ from: gradient[0], to: gradient[1], deg: 90 }}
