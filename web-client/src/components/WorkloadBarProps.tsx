5f5c01031d5dc080d3d22fa8269d27d568e5f418
virem <viremaydin@gmail.com>
2023-12-11 16:53:11 +0300

    feat: feat: PRCreationPage.tsx, WorkloadBar.tsx

--- web-client/src/components/WorkloadBarProps.tsx
+++ web-client/src/components/WorkloadBarProps.tsx
@@ -4,8 +4,8 @@ interface WorkloadBarProps {
   workload: number;
 }
 function WorkloadBarProps({ workload }: WorkloadBarProps){
-  const color : string = workload > 70 ? "red" :
-    workload > 50 ? "yellow" :
+  const color : string = workload > 80 ? "red" :
+    workload > 60 ? "orange" : workload > 40 ?"yellow" :
       "green";
   return(
     <Box w={"130px"} m={0}>
