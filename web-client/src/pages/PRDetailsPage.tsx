d1e2162bac1675a888c981b7e656bc418e0eb410
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2023-12-13 11:44:04 +0300

    Pr detail context and sidebar (#11)

--- web-client/src/pages/PRDetailsPage.tsx
+++ web-client/src/pages/PRDetailsPage.tsx
@@ -1,10 +1,13 @@
 import LabelButton from "../components/LabelButton";
 import ModifiedFilesTab from "../tabs/ModifiedFilesTab";
 import CommentsTab from "../tabs/CommentsTab.tsx";
-import { Box, Badge, rem } from "@mantine/core";
+import {Box, Badge, rem, Grid, Text} from "@mantine/core";
 import TabComp from "../components/Tab.tsx";
 import * as React from "react";
-import {IconGitPullRequest, IconCircleDot } from '@tabler/icons-react';
+import {IconGitPullRequest, IconCircleDot, IconUserPlus, IconCheck } from '@tabler/icons-react';
+import PrContextTab from "../tabs/PrContextTab.tsx";
+import UserLogo from "../assets/icons/user.png";
+import classes from "../styles/PRList.module.css";
 
 interface PRDetailsPageProps {
   // Define the props you want to pass to PrDetailPage
@@ -51,13 +54,51 @@ function PRDetailsPage({id, name}: PRDetailsPageProps) {
       </p>
 
       <br></br>
+      <Grid>
+        <Grid.Col span={7.5}>
+          <TabComp tabs={tabs} updateTab={updateTab} />
+          <Box style={{width:900}}> current tab is {currentTab}
+            {currentTab === "modified files" && <ModifiedFilesTab />}
+            {currentTab === "comments" && <CommentsTab />}
+            {currentTab === "details" && <PrContextTab />}
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={3}>
+          <Box style={{border:"solid 0.5px cyan", borderRadius:"10px", padding: "8px"}}>
+            <Text size="lg" style={{style:"bold"}}> Reviewers </Text>
+            <Box style={{display:"flex", marginBottom:"3px"}}>
+              <Box component="img" src={UserLogo} alt={"logo"} className={classes.logo} />
+              <Text size={"md"} style={{padding: "3px"}}>irem_aydın</Text> &ensp;
+              <IconCheck size={24} strokeWidth={3} color={"green"}/>
+            </Box>
+            <br></br>
+            <Box style={{display:"flex" }}>
+              <Text size="lg" style={{style:"bold"}}> Assignees </Text>
+              <IconUserPlus style={{ width: rem(18), height: rem(18), alignItems:"right"}} />
+            </Box>
+            <Box style={{display:"flex", marginBottom:"3px"}}>
+              <Box component="img" src={UserLogo} alt={"logo"} className={classes.logo} />
+              <Text size={"md"} style={{padding: "3px"}}>ecekahraman</Text>
+            </Box>
 
-      <TabComp tabs={tabs} updateTab={updateTab} />
-      <Box style={{width:900}}> current tab is {currentTab}
-        {currentTab === "modified files" && <ModifiedFilesTab />}
-        {currentTab === "comments" && <CommentsTab />}
-      </Box>
+            <Box style={{display:"flex", marginBottom:"3px"}}>
+              <Box component="img" src={UserLogo} alt={"logo"} className={classes.logo} />
+              <Text size={"md"} style={{padding: "3px"}}>irem_aydın</Text>
+            </Box>
 
+            <br></br>
+
+            <Text size="lg" style={{style:"bold"}}> Number of req. approves </Text>
+            <Box style={{display:"flex", marginBottom:"3px"}}>
+              <Badge size={"lg"} color={""} style={{marginTop:10}}>
+                1/3
+              </Badge>
+            </Box>
+
+
+          </Box>
+        </Grid.Col>
+      </Grid>
     </div>
   );
 }
