92a9c33ac829aa2fd5a4680fb5beb2f9ddb70cfb
Alper Mumcular <72230736+AlperMumcular@users.noreply.github.com>
2023-12-14 21:44:17 +0300

    File Grouping (#14)

---
+++ web-client/src/components/FileGrouping.tsx
@@ -0,0 +1,37 @@
+import UserLogo from "../assets/icons/user.png";
+import GitHubLogo from "../assets/icons/github-mark-white.png";
+import {Text, Box, Flex, Paper, Group, Button} from '@mantine/core';
+import classes from "../styles/PRList.module.css";
+import LabelButton from "./LabelButton";
+import {Link} from "react-router-dom";
+import PriorityBadge from "./PriorityBadge";
+
+interface FileGroupingProps {
+  name: string;
+  id:number;
+  files: string[];
+  reviewers: string [];
+}
+
+function FileGrouping({files, reviewers}: FileGroupingProps) {
+
+  return(
+    <Paper w={"300px"} h={"200px"} withBorder>
+      <Group>
+        <Text>Files</Text>
+        {
+          files.map(itm=>(
+            <Text key={1}>{itm}</Text>
+          ))
+        }
+      </Group>
+
+      <Group>
+        <Button color={"red"}>Delete</Button>
+        <Button>Edit</Button>
+      </Group>
+  </Paper>)}
+
+
+export default FileGrouping;
+
