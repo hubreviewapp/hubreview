59bc11caea24d19582a3da452716b3c5a676c97f
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2024-01-21 20:29:49 +0300

    feat: merge button for Pr

--- web-client/src/components/SplitButton.tsx
+++ web-client/src/components/SplitButton.tsx
@@ -1,45 +1,115 @@
 import {Button, Menu, Group, ActionIcon, rem, Text} from '@mantine/core';
+import {Box, Popover} from "@mantine/core";
 import {IconChevronDown} from '@tabler/icons-react';
 import classes from './SplitButton.module.scss';
+import { IconGitMerge, IconX, IconCheck } from '@tabler/icons-react';
 
 function SplitButton() {
   return (
-    <Group wrap="nowrap" gap={0}>
-      <Button color="green" className={classes.button}>Merge Pull Request</Button>
-      <Menu transitionProps={{transition: 'pop'}} position="bottom-end" withinPortal>
-        <Menu.Target>
-          <ActionIcon
-            variant="filled"
-            color="green"
-            size={36}
-            className={classes.menuControl}
-          >
-            <IconChevronDown style={{width: rem(16), height: rem(16)}} stroke={1.5}/>
-          </ActionIcon>
-        </Menu.Target>
-        <Menu.Dropdown>
-          <Menu.Item>
-            Create a merge commit
-            <Text c="dimmed" mt="xs" fz="xs" w="250px">
-                All commits from this branch will be added
-              to the base branch via a merge commit.
+    <div className="popover-container">
+      <Popover
+        withArrow arrowOffset={14} arrowSize={15}
+        width={200}
+        position="right-start"
+        opened
+        offset={{ mainAxis: 15, crossAxis: 0 }}
+
+      >
+        <Popover.Target >
+          <Box style={{position: "relative",backgroundColor: 'dimgray', width: 70, borderRadius: 10}}>
+            <IconGitMerge
+              style={{ width: rem(50), height: rem(50), marginTop: 0, marginLeft: 10}} />
+          </Box>
+        </Popover.Target>
+        <Popover.Dropdown style={{ width: 800}}>
+          <Box style={{display: 'flex'}}>
+            <Box>
+            <IconX
+              color="red" style= {{width: rem(30), height: rem(30), marginLeft: 5, marginTop: 10, }} />
+            </Box>
+            <Text color="red" fw={700} style={{marginTop: 15}}>
+              Review required (2/3)
             </Text>
-          </Menu.Item>
-          <Menu.Item>
-            Squash and merge
-            <Text mt="xs" c="dimmed" fz="xs" w="250px">
-             The 1 commit from this branch will be added to the base branch.
+          </Box>
+          <Text size="sm">
+            At least 3 approving review is required by reviewers with write access.
+          </Text>
+          <Text size="sm" td="underline" c="blue">
+            See all reviewers
+          </Text>
+          <hr></hr>
+          <Box style={{display: 'flex'}}>
+            <Box>
+              <IconCheck
+                color= "green" style={{ width: rem(30), height: rem(30), marginLeft: 5, marginTop: 10, }} />
+            </Box>
+            <Text fw={700} style={{marginTop: 15}}>
+              All checks have passed
             </Text>
-          </Menu.Item>
-          <Menu.Item>
-            Rebase and merge
-            <Text mt="xs" c="dimmed" fz="xs" w="250px">
-              The 1 commit from this branch will be rebased and added to the base branch.
+          </Box>
+          <Text size="sm">
+            2 successful checks
+          </Text>
+          <hr></hr>
+          <Box style={{display: 'flex'}}>
+            <Box>
+              <IconX
+                color= "red" style={{ width: rem(30), height: rem(30), marginLeft: 5, marginTop: 10, }} />
+            </Box>
+            <Text color="red" style={{marginTop: 15}}>
+              Merging is blocked
             </Text>
-          </Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
-    </Group>
+          </Box>
+          <Text size="sm">
+            Merging can be performed automatically with 3 approving reviews.
+          </Text>
+
+          <br></br>
+          <Group wrap="nowrap" gap={0} style={{border: "1px groove gray", width:250}}>
+            <Button disabled color="green" className={classes.button} >Merge Pull Request</Button>
+            <Menu transitionProps={{transition: 'pop'}} position="bottom-end" withinPortal>
+              <Menu.Target>
+                <ActionIcon
+                  disabled
+                  variant="filled"
+                  color="gray"
+                  size={36}
+                  className={classes.menuControl}
+                >
+                  <IconChevronDown style={{width: rem(16), height: rem(16)}} stroke={1.5}/>
+                </ActionIcon>
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Item>
+                  Create a merge commit
+                  <Text c="dimmed" mt="xs" fz="xs" w="250px">
+                    All commits from this branch will be added
+                    to the base branch via a merge commit.
+                  </Text>
+                </Menu.Item>
+                <Menu.Item>
+                  Squash and merge
+                  <Text mt="xs" c="dimmed" fz="xs" w="250px">
+                    The 1 commit from this branch will be added to the base branch.
+                  </Text>
+                </Menu.Item>
+                <Menu.Item>
+                  Rebase and merge
+                  <Text mt="xs" c="dimmed" fz="xs" w="250px">
+                    The 1 commit from this branch will be rebased and added to the base branch.
+                  </Text>
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
+
+
+        </Popover.Dropdown>
+      </Popover>
+
+      <Box style={{height: 270}}></Box>
+
+    </div>
   );
 }
 
