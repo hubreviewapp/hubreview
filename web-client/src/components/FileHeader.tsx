1a7ac560ce957c1c8454e10b7858ac6455082617
Vesile İrem Aydın <67067747+vIremAydin@users.noreply.github.com>
2024-01-28 18:19:04 +0300

    feature: add diff files header comp (#32)
    Header added to show details of diff files including the number of
    added/deleted lines, resolved and active comments, and last update date.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a File Header display, providing details such as file name,
    author, last update, and statistics on lines added/deleted and comment
    counts.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ web-client/src/components/FileHeader.tsx
@@ -0,0 +1,42 @@
+import {Box, Paper, Title, Flex, Text, UnstyledButton, Divider, rem} from "@mantine/core";
+import {IconMessage} from "@tabler/icons-react";
+
+interface FileHeaderProps {
+  fileName: string,
+  author: string,
+  lastUpdate: Date,
+  numOfAddedLines: number,
+  numOfDeletedLines: number,
+  numberOfResolvedComments: number,
+  numberOfActiveComments: number,
+
+}
+function FileHeader({
+                      fileName, author, lastUpdate,
+                      numOfAddedLines,
+                      numOfDeletedLines,
+                      numberOfResolvedComments,
+                      numberOfActiveComments
+                    }:FileHeaderProps) {
+
+  return (
+    <Paper w="100%" h="100px">
+      <Box my="sm">
+        <Title order={3} mb="sm">{fileName}</Title>
+        <Flex gap="md">
+          <Text c="dimmed">Last updated on {lastUpdate.toDateString()} by {}
+            <UnstyledButton c="blue">{author}</UnstyledButton></Text>
+          <Divider orientation="vertical"/>
+          <Text c="green"> + {numOfAddedLines}</Text>
+          <Text c="red"> - {numOfDeletedLines}</Text>
+          <Divider orientation="vertical"/>
+          <IconMessage style={{ width: rem(22), height: rem(22)}} />
+          {numberOfActiveComments} / {numberOfResolvedComments} Resolved
+        </Flex>
+      </Box>
+      <Divider />
+    </Paper>
+  );
+}
+
+export default FileHeader;
