44c070204051ea366178fa8120424c3aebe98c9f
virem <viremaydin@gmail.com>
2024-02-01 08:53:40 +0300

    refactor: RepositoriesPage.tsx

--- web-client/src/pages/RepositoriesPage.tsx
+++ web-client/src/pages/RepositoriesPage.tsx
@@ -1,83 +1,63 @@
 import "../styles/temp-styles.module.css";
-import GitHubLogo from "../assets/icons/github-mark-white.png";
-import { Flex, Box, Card, Group, Text, Button, Badge, rem, Modal, Switch } from "@mantine/core";
-import { IconLock } from "@tabler/icons-react";
-import { useState } from "react";
-import { useDisclosure } from "@mantine/hooks";
+import {
+  Flex,
+  Box,
+  Table,
+  UnstyledButton,
+  Text,
+  Button,
+  Paper,
+  rem,
+  Title,
+  TextInput
+} from "@mantine/core";
+import {IconCirclePlus} from "@tabler/icons-react";
+import {useState} from "react";
 import repoData from "../repo_data.json";
 
 function RepositoriesPage() {
-  const repos = repoData;
-  const [selectedRepos, setSelectedRepos] = useState<string[]>(["ReLink"]);
-  const iconLock = <IconLock style={{ width: rem(12), height: rem(12) }} />;
-  const [opened, { open, close }] = useDisclosure(false);
+  const iconPlus = <IconCirclePlus style={{width: rem(22), height: rem(22)}}/>;
+  const [query, setQuery] = useState('');
+  const filtered = repoData.filter((item) => item.name.toLowerCase().includes(query.toLowerCase()));
+
+  const rows = filtered.map((element) => (
+    <Table.Tr key={element.id}>
+      <Table.Td><Text fw="700">{element.name}</Text></Table.Td>
+      <Table.Td c="dimmed">created by <UnstyledButton c="blue">{element.owner}</UnstyledButton> on {element.created}
+      </Table.Td>
+      <Table.Td><Button variant="light">Configure</Button></Table.Td>
+    </Table.Tr>
+  ));
 
   return (
-    <Box h={600} p={5} m={0} w="100%">
-      <Modal opened={opened} onClose={close} title="Add GitHub Repositories to HubReview">
-        <Switch.Group value={selectedRepos} onChange={setSelectedRepos}>
-          {repos.map((itm) => (
-            <Switch
-              key={itm.id}
-              value={itm.name}
-              label={
-                <Text>
-                  {itm.name}{" "}
-                  <Text size="sm" c="dimmed">
-                    created by {itm.owner}{" "}
-                  </Text>
-                </Text>
-              }
-              m="md"
-            />
-          ))}
-        </Switch.Group>
-        <Box>
-          <Button color="gray" onClick={close}>
-            Close
-          </Button>
-        </Box>
-      </Modal>
-      <Flex p={5} mt="20px" justify="center">
-        {selectedRepos.length == 0 ? (
-          <Text size="lg">You have no repository here.</Text>
-        ) : (
-          selectedRepos
-            .map((i) => repos.find((r) => r.name === i))
-            .filter((i): i is (typeof repos)[0] => i !== undefined)
-            .map((repo) => (
-              <Card key={repo.id} w="300px" h="170px" shadow="sm" padding="lg" radius="md" m={5} bg="#0D1B2A">
-                <Flex direction="column" justify="space-between" m={5}>
-                  <Flex justify="space-between">
-                    <Text fw={500} size="xl">
-                      {repo.name}
-                    </Text>
-                    <Badge rightSection={iconLock} variant="outline" color="gray" size="sm" mt={4}>
-                      Private
-                    </Badge>
-                  </Flex>
-                  <Text size="sm" c="dimmed">
-                    Owner: {repo.owner}
-                  </Text>
-                  <Text size="sm" c="dimmed">
-                    Created: {repo.created}
-                  </Text>
-                </Flex>
-                <Group justify="flex-end" m={5}>
-                  <Button size="xs" variant="outline" color="white">
-                    Edit
-                  </Button>
-                </Group>
-              </Card>
-            ))
-        )}
-      </Flex>
-      <Flex justify="center" mt="30px">
-        <Button onClick={open}>
-          Add Repository
-          <Box component="img" src={GitHubLogo} alt="logo" ml="20px" w="25px" />
+    <Box p="lg">
+      <Flex justify="space-between" my="md">
+        <Title order={3}>Repositories</Title>
+        <Button size="md" leftSection={iconPlus}>
+          Add Repositories
         </Button>
       </Flex>
+      <Text c="dimmed">These are all the repositories that HubReview can access. To add a repository, you will be
+        directed to GitHub.
+      </Text>
+      <Paper my="lg">
+        <Box w="300px">
+          <TextInput
+            my="md"
+            label="Search for repository"
+            radius="md"
+            value={query}
+            onChange={(event) => {
+              setQuery(event.currentTarget.value);
+            }}
+            placeholder="Search Repository"
+          />
+        </Box>
+
+        <Table verticalSpacing="md" striped>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </Paper>
     </Box>
   );
 }
