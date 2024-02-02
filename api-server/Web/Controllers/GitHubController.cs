530a5ff13b097ce3f02bdbf3800c7ae1b469e1c2
Alper Mumcular <alper.mumcular@ug.bilkent.edu.tr>
2024-02-02 15:22:54 +0300

    repo get

--- api-server/Web/Controllers/GitHubController.cs
+++ api-server/Web/Controllers/GitHubController.cs
@@ -1,5 +1,6 @@
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using Octokit;
 
 namespace CS.Web.Controllers;
 
@@ -8,13 +9,13 @@ namespace CS.Web.Controllers;
 
 public class GitHubController : ControllerBase
 {
-    [HttpGet("getToken")]
-    public async Task<ActionResult> getToken(string code)
+    [HttpGet("acquireToken")]
+    public async Task<ActionResult> acquireToken(string code)
     {
 
         var clientId = "64318456282bb1488063";
         var clientSecret = "51388c5053ea503fc8141b0184b05b5ae5529b81";
-        var redirectUri = "http://localhost:5018/api/github/getToken";
+        var redirectUri = "http://localhost:5018/api/github/acquireToken";
         
         using (var httpClient = new HttpClient())
         {
@@ -38,9 +39,28 @@ public class GitHubController : ControllerBase
             HttpContext.Session.SetString("AccessToken", access_token);
             //var accessToken = HttpContext.Session.GetString("AccessToken");
 
-            //Console.WriteLine("session: " + sessionnn);
-
             return Redirect($"http://localhost:5173");
         }
+    }
+
+
+    [HttpGet("repos")]
+    public async Task<ActionResult> getRepository()
+    {
+        var access_token = HttpContext.Session.GetString("AccessToken");
+        Console.WriteLine(access_token);
+
+        //Console.WriteLine("session: " + sessionnn);
+        var client1 = new GitHubClient(new ProductHeaderValue("HubReview"));
+        var tokenAuth = new Credentials(access_token);
+        client1.Credentials = tokenAuth;
+        var user = await client1.User.Current();
+        var repos = await client1.Repository.GetAllForUser(user.Login);
+        foreach (var repo in repos)
+        {
+            Console.WriteLine(repo.Name);
+        }
+
+        return Redirect($"http://localhost:5173");
     }
 }
