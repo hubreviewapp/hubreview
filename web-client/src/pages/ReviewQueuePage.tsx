3595a04c0310dc3ff476c2d5e70bb3ed86f2a585
Vesile İrem Aydın <67067747+vIremAydin@users.noreply.github.com>
2024-02-20 08:58:21 +0300

    get pr info from github (#51)

--- web-client/src/pages/ReviewQueuePage.tsx
+++ web-client/src/pages/ReviewQueuePage.tsx
@@ -1,12 +1,12 @@
-import { Grid, Button, Center} from "@mantine/core";
+import {Grid, Button, Center} from "@mantine/core";
 import FilterInput from "../components/ReviewQueue/FilterInput";
 import {Link} from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useListState } from '@mantine/hooks';
-import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useListState} from '@mantine/hooks';
+import {useNavigate} from "react-router-dom";
 import {PRNavbar} from "../components/ReviewQueue/PRNavbar.tsx";
 import PRCardList from "../components/ReviewQueue/PRCardList";
-import { PRInfo } from "../models/PRInfo";
+import {PRInfo} from "../models/PRInfo";
 import axios from "axios";
 
 
@@ -82,13 +82,13 @@ function ReviewQueuePage() {
 
   const [prInfo, setPrInfo] = useState<PRInfo[]>([]);
   const navigate = useNavigate();
-  const [activeSection, setActiveSection] = useState<string >('');
+  const [activeSection, setActiveSection] = useState<string>('');
 
   const [values, handlers] = useListState<SelectedRepos>([]);
 
   useEffect(() => {
 
-    if ( localStorage.getItem("userLogin") === null ){
+    if (localStorage.getItem("userLogin") === null) {
       navigate("/signIn");
     }
 
@@ -114,35 +114,34 @@ function ReviewQueuePage() {
   }, []);
 
   return (
-    <Grid w="100%" mt="md">
-      <Grid.Col span={3} >
-        <div style={{ position: 'sticky', top:5 }}>
-          <PRNavbar setActiveSection={setActiveSection} activeSection={activeSection}  selectedRepos={values}
-                    setSelectedRepos={handlers} />
+    <Grid mt="md">
+      <Grid.Col span={3}>
+        <div style={{position: 'sticky', top: 5}}>
+          <PRNavbar setActiveSection={setActiveSection} activeSection={activeSection} selectedRepos={values}
+                    setSelectedRepos={handlers}/>
         </div>
       </Grid.Col>
 
-      <Grid.Col span={8}>
-        <FilterInput />
-        <div id="needs-your-review" >
-          <PRCardList pr={prInfo} name="Needs Your Review" />
-        </div>
-        <div id="your-prs" >
-          <PRCardList pr={[]} name="Your PRs" />
-        </div>
-        <div id="waiting-for-author" >
-          <PRCardList pr={[]} name="Waiting for author" />
-        </div>
-        <div id="all-open-prs" >
-          <PRCardList pr={prInfo} name="All Open PRs" />
-        </div>
-        <div id="merged" >
-          <PRCardList pr={[]} name="merged" />
-        </div>
-        <div id="closed">
-          <PRCardList pr={prInfo} name="closed" />
-        </div>
-
+      <Grid.Col span={8} ml="md">
+        <FilterInput/>
+          <div id="needs-your-review">
+            <PRCardList pr={prInfo} name="Needs Your Review"/>
+          </div>
+          <div id="your-prs">
+            <PRCardList pr={[]} name="Your PRs"/>
+          </div>
+          <div id="waiting-for-author">
+            <PRCardList pr={[]} name="Waiting for author"/>
+          </div>
+          <div id="all-open-prs">
+            <PRCardList pr={prInfo} name="All Open PRs"/>
+          </div>
+          <div id="merged">
+            <PRCardList pr={[]} name="Merged"/>
+          </div>
+          <div id="closed">
+            <PRCardList pr={prInfo} name="Closed"/>
+          </div>
         <Center>
           <Link to="/createPR">
             <Button m="lg">Create New PR</Button>
