ceb94b953ca030171db0ef982ac9834bc1c989dd
Alper Mumcular <alper.mumcular@ug.bilkent.edu.tr>
2024-02-23 23:02:55 +0300

    List Repo Labels

--- api-server/Web/Controllers/GitHubController.cs
+++ api-server/Web/Controllers/GitHubController.cs
@@ -775,4 +775,53 @@ public class GitHubController : ControllerBase
         return NotFound("There exists no user in session.");
     }
 
+    [HttpGet("getRepoLabels/{owner}/{repoName}")]
+    public async Task<ActionResult> GetRepoLabels(string owner, string repoName)
+    {
+        var appClient = GetNewClient();
+        var userClient = GetNewClient(_httpContextAccessor?.HttpContext?.Session.GetString("AccessToken"));
+        var userLogin = _httpContextAccessor?.HttpContext?.Session.GetString("UserLogin");
+
+        // Get organizations for the current user
+        var organizations = await userClient.Organization.GetAllForCurrent();
+        var organizationLogins = organizations.Select(org => org.Login).ToArray();
+
+        var result = new List<LabelInfo>();
+
+        var installations = await appClient.GitHubApps.GetAllInstallationsForCurrent();
+        foreach (var installation in installations)
+        {
+            if (installation.Account.Login == userLogin || organizationLogins.Contains(installation.Account.Login))
+            {
+                var response = await appClient.GitHubApps.CreateInstallationToken(installation.Id);
+                var installationClient = GetNewClient(response.Token);
+
+                try
+                {
+                    var labels = await installationClient.Issue.Labels.GetAllForRepository(owner, repoName);
+
+                    foreach (var label in labels)
+                    {
+                        result.Add(new LabelInfo
+                        {
+                            id = label.Id,
+                            name = label.Name,
+                            color = label.Color
+                        });
+                    }
+
+                    return Ok(result);
+                }
+                catch (NotFoundException)
+                {
+                    return NotFound($"Repository {repoName} not found under owner {owner}.");
+                }
+            }
+        }
+        return NotFound("There exists no user in session.");
+    }
+
+
 }
+
+
