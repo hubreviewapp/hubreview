c22b7249fab340f7aa7b62269cbc792c1931e7f0
Ved <dev@vedat.xyz>
2024-02-26 11:00:15 +0300

    feat: base flow for the reviews tab
    Still needs a fair amount of refactors and improvements

--- web-client/src/components/DiffComment.tsx
+++ web-client/src/components/DiffComment.tsx
@@ -1,27 +1,36 @@
 import { Avatar, Badge, Button, Divider, Group, Paper, Text, TextInput } from "@mantine/core";
 import classes from "../styles/comment.module.css";
 import UserLogo from "../assets/icons/user.png";
+import { ReviewComment } from "../tabs/ModifiedFilesTab";
 
-function DiffComment() {
+export interface DiffCommentProps {
+  comment: ReviewComment;
+  isPending: boolean;
+}
+
+function DiffComment({ comment, isPending }: DiffCommentProps) {
   return (
     <Paper withBorder radius="md" className={classes.comment} shadow="lg">
       <Divider my={9} />
       <Group>
         <Badge size="md" color="#888" key={1} mb={4}>
-          nit
+          {comment.label}
         </Badge>
 
-        <Badge size="md" color="green" key={1} mb={4}>
-          non-blocking
+        <Badge size="md" color="green" key={2} mb={4}>
+          {comment.decoration}
         </Badge>
-        <Badge size="md" color="purple" key={1} mb={4}>
-          style
-        </Badge>
+
+        {isPending && (
+          <Badge size="md" color="orange" variant="light" key={3} mb={4}>
+            Pending
+          </Badge>
+        )}
       </Group>
       <Group>
         <Avatar src={UserLogo} alt="Jacob Warnhalter" radius="xl" />
         <div>
-          <Text fz="sm">aysekelleci</Text>
+          <Text fz="sm">AUTHOR</Text>
           <Text fz="xs" c="dimmed">
             {new Date(2023, 4, 7).toLocaleString("en-US", {
               day: "numeric",
@@ -32,23 +41,26 @@ function DiffComment() {
           </Text>
         </div>
       </Group>
-      <h5>
-        This could be wrapped in a Boolean to make sure the type is a boolean instead of string. Isn't that in our
-        latest style guideline, @ecekahraman?
-      </h5>
+      <h5>{comment.content}</h5>
       <div />
 
       <TextInput placeholder="Reply..."></TextInput>
 
-      <Divider my={10} />
-      <Group justify="start" mt={5}>
-        <Button size="sm" color="green">
-          Resolve comment
-        </Button>
+      {!isPending && (
+        <>
+          <Divider my={10} />
+          <Group justify="start" mt={5}>
+            <Button size="sm" color="green">
+              Resolve comment
+            </Button>
+            {/*
         <Text c="gray" size="sm">
           ecekahraman was requested to review this comment
         </Text>
-      </Group>
+        */}
+          </Group>
+        </>
+      )}
     </Paper>
   );
 }
