faddc93352e7c96e34e83de77893749d3b314e81
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2024-03-10 20:50:05 +0300

    Navigate signin and pr filter UI (#88)

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -13,6 +13,7 @@ import ReviewQueuePage from "./pages/ReviewQueuePage.tsx";
 import UserProvider from "./providers/UserProvider.tsx";
 import NoRenderOnPath from "./utility/NoRenderOnPath";
 import "@mantine/tiptap/styles.css";
+import "./styles/filter.css";
 
 function App() {
   return (
--- web-client/src/pages/AnalyticsPage.tsx
+++ web-client/src/pages/AnalyticsPage.tsx
@@ -3,18 +3,8 @@ import ReviewBuddyBox from "../components/ReviewBuddyBox";
 import ApprovalRejectionRates from "../components/ApprovalRejectionRates";
 import WorkloadAnalytics from "../components/WorkloadAnalytics";
 import ApprovalRejectionRatesForReviewer from "../components/ApprovalRejectionRatesForReviewer.tsx";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 function AnalyticsPage() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem("userLogin") === null) {
-      navigate("/signIn");
-    }
-  }, [navigate]);
-
   return (
     <Container size="responsive">
       <Group style={{ justifyContent: "center" }}>
