41fc920b7373d4a36f1d74a0c7ca02652eaeadb4
Ved <dev@vedat.xyz>
2024-03-16 23:56:03 +0300

    chore: prettier sweep

--- web-client/src/components/Analytics/ReviewLineChart.tsx
+++ web-client/src/components/Analytics/ReviewLineChart.tsx
@@ -1,49 +1,50 @@
-import { LineChart } from '@mantine/charts';
-import { Paper, Title} from "@mantine/core";
+import { LineChart } from "@mantine/charts";
+import { Paper, Title } from "@mantine/core";
 const data = [
   {
-    date: 'Mar 22',
+    date: "Mar 22",
     Submitted: 9,
     Received: 8,
     Waiting: 2,
   },
   {
-    date: 'Mar 23',
+    date: "Mar 23",
     Submitted: 12,
     Received: 6,
     Waiting: 4,
   },
   {
-    date: 'Mar 24',
+    date: "Mar 24",
     Submitted: 8,
     Received: 11,
     Waiting: 3,
   },
   {
-    date: 'Mar 25',
+    date: "Mar 25",
     Submitted: 9,
     Received: 8,
     Waiting: 2,
   },
-
 ];
 function ReviewLineChart() {
   return (
-  <Paper p='md' ta="center">
-    <Title order={4} mb="sm">Review Chart</Title>
-    <LineChart
-      h={300}
-      data={data}
-      dataKey="date"
-      series={[
-        { name: 'Submitted', color: 'indigo.6' },
-        { name: 'Received', color: 'yellow.6' },
-        { name: 'Waiting', color: 'gray.6' },
-      ]}
-      curveType="linear"
-      withLegend
-    />
-  </Paper>
+    <Paper p="md" ta="center">
+      <Title order={4} mb="sm">
+        Review Chart
+      </Title>
+      <LineChart
+        h={300}
+        data={data}
+        dataKey="date"
+        series={[
+          { name: "Submitted", color: "indigo.6" },
+          { name: "Received", color: "yellow.6" },
+          { name: "Waiting", color: "gray.6" },
+        ]}
+        curveType="linear"
+        withLegend
+      />
+    </Paper>
   );
 }
 
--- web-client/src/pages/AnalyticsPage.tsx
+++ web-client/src/pages/AnalyticsPage.tsx
@@ -1,6 +1,6 @@
-import {Box, Button, SimpleGrid, Grid, Paper, Title, Container, Divider, Flex} from "@mantine/core";
+import { Box, Button, SimpleGrid, Grid, Paper, Title, Container, Divider, Flex } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import ReviewSummaryAnalytics from "../components/Analytics/ReviewSummaryAnalytics";
 import ReviewLineChart from "../components/Analytics/ReviewLineChart";
 import axios from "axios";
@@ -8,7 +8,7 @@ import ReviewerSpeedAnalytics from "../components/Analytics/ReviewerSpeedAnalyti
 
 function AnalyticsPage() {
   const navigate = useNavigate();
-  const [repoList, setRepoList]  =useState([]);
+  const [repoList, setRepoList] = useState([]);
   useEffect(() => {
     if (localStorage.getItem("userLogin") === null) {
       navigate("/signIn");
@@ -17,12 +17,9 @@ function AnalyticsPage() {
   useEffect(() => {
     const getRepos = async () => {
       try {
-        const res = await axios.get(
-          `http://localhost:5018/api/github/getRepository`,
-          {
-            withCredentials: true,
-          },
-        );
+        const res = await axios.get(`http://localhost:5018/api/github/getRepository`, {
+          withCredentials: true,
+        });
         if (res.data) {
           setRepoList(res.data.repoNames);
         }
@@ -36,36 +33,38 @@ function AnalyticsPage() {
 
   return (
     <Container fluid my="md">
-      <Box mb="sm" align="center" >
+      <Box mb="sm" align="center">
         <Title order={3}>Dashboard</Title>
       </Box>
       <SimpleGrid cols={{ base: 1, sm: 1 }} spacing="md">
         <Grid gutter="md">
           <Grid.Col span={5}>
-            <ReviewSummaryAnalytics/>
+            <ReviewSummaryAnalytics />
           </Grid.Col>
-          <Divider orientation="vertical"/>
+          <Divider orientation="vertical" />
           <Grid.Col span={6}>
-            <ReviewLineChart/>
+            <ReviewLineChart />
           </Grid.Col>
         </Grid>
-        <Divider/>
+        <Divider />
         <Grid gutter="md">
           <Grid.Col span={5}>
             <Paper p="md" ta="center">
-              <Title order={4} mb="md">Repository Analytics</Title>
+              <Title order={4} mb="md">
+                Repository Analytics
+              </Title>
               <Flex direction="column" justify="center" align="center">
-              {
-                repoList.map(r=>(
-                  <Button key={r.id} mb="sm" w="50%" variant="outline" color="blue">{r.name}</Button>
-                ))
-              }
+                {repoList.map((r) => (
+                  <Button key={r.id} mb="sm" w="50%" variant="outline" color="blue">
+                    {r.name}
+                  </Button>
+                ))}
               </Flex>
             </Paper>
           </Grid.Col>
-          <Divider orientation="vertical"/>
+          <Divider orientation="vertical" />
           <Grid.Col span={6}>
-            <ReviewerSpeedAnalytics/>
+            <ReviewerSpeedAnalytics />
           </Grid.Col>
         </Grid>
       </SimpleGrid>
