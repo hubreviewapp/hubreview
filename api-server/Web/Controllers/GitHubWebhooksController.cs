4d798875758cb6084d91d1e2f98599a82ddb36da
Ece Kahraman <ece.kahraman@ug.bilkent.edu.tr>
2024-04-04 00:35:12 +0300

    feat: installation webhook considers comments

--- api-server/Web/Controllers/GitHubWebhooksController.cs
+++ api-server/Web/Controllers/GitHubWebhooksController.cs
@@ -188,17 +188,13 @@ namespace CS.Web.Controllers
 
                             connection.Open();
 
-                            string query = "INSERT INTO repositoryinfo (id, node_id, name, ownerLogin, created_at) VALUES (@id, @node_id, @repoName, @owner, @created_at)";
+                            string query = $"INSERT INTO repositoryinfo (id, node_id, name, ownerLogin, created_at) VALUES ({id}, '{node_id}', '{repoName}', '{owner}', '{repo.CreatedAt.Date:yyyy-MM-dd}')";
+                            string comm_query = "INSERT INTO comments (commentid, reponame, prnumber, is_review) VALUES";
+                            string review_head_query = "INSERT INTO reviewhead (review_id, reponame, prnumber, body, verdict, comments) VALUES";
 
                             // GetRepository by id lazım...
                             using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
                             {
-                                command.Parameters.AddWithValue("@id", repository.id);
-                                command.Parameters.AddWithValue("@node_id", repository.node_id);
-                                command.Parameters.AddWithValue("@owner", owner);
-                                command.Parameters.AddWithValue("@repoName", repoName);
-                                command.Parameters.AddWithValue("@created_at", repo.CreatedAt.Date.ToString("yyyy-MM-dd"));
-
                                 command.ExecuteNonQuery();
                             }
 
@@ -342,29 +338,44 @@ namespace CS.Web.Controllers
                                     var installationReviewsJson = JsonConvert.SerializeObject(installationLatestReviews);
 
 
-                                    query += $"({repository.id}, {pull.Id}, '{pull.Base.Repository.Name}', {pull.Number}, '{pull.Title}', '{pull.User.Login}', '{pull.User.AvatarUrl}', '{pull.CreatedAt.Date:YYYY-MM-DD}', '{pull.UpdatedAt.Date:YYYY-MM-DD}', {pull.Comments}, {pull.Commits}, {pull.ChangedFiles}, {pull.Additions}, {pull.Deletions}, {pull.Draft}, {pull.Merged}, '{pull.State.ToString()}', {requestedReviewers}, '{labeljson}', '{pull.Url}', '{pull.Base.Repository.Owner.Login}', '{JsonConvert.SerializeObject(checksList)}', {checks_complete_count}, {checks_incomplete_count}, {checks_success_count}, {checks_fail_count}, {assignedReviewers}, '{installationReviewsJson}', {priority}), ";
+                                    query += $"({repository.id}, {pull.Id}, '{pull.Base.Repository.Name}', {pull.Number}, '{pull.Title}', '{pull.User.Login}', '{pull.User.AvatarUrl}', '{pull.CreatedAt.Date:yyyy-MM-dd}', '{pull.UpdatedAt.Date:yyyy-MM-dd}', {pull.Comments}, {pull.Commits}, {pull.ChangedFiles}, {pull.Additions}, {pull.Deletions}, {pull.Draft}, {pull.Merged}, '{pull.State.StringValue}', {requestedReviewers}, '{labeljson}', '{pull.Url}', '{pull.Base.Repository.Owner.Login}', '{JsonConvert.SerializeObject(checksList)}', {checks_complete_count}, {checks_incomplete_count}, {checks_success_count}, {checks_fail_count}, {assignedReviewers}, '{installationReviewsJson}', {priority}), ";
                                     
                                     var comments = await installationClient.Issue.Comment.GetAllForIssue(repository.id, repoPull.Number);
-                                    string comm_query = "INSERT INTO comments VALUES";
                                     foreach (var comm in comments)
                                     {
-                                        comm_query += $" ({comm.Id}, {repository.name}, {pull.Number}, {false}, '', '', ''),";
+                                        comm_query += $" ({comm.Id}, '{repository.name}', {pull.Number}, {false}),";
                                     }
 
-                                    var reviewcomments = await installationClient.PullRequest.ReviewComment.GetAll(repository.id, repoPull.Number);
-                                    foreach (var revcomm in reviewcomments)
+                                    var reviewheads = await installationClient.PullRequest.Review.GetAll(owner, repoName, pull.Number);
+                                    foreach (var review in reviewheads)
                                     {
-                                        comm_query += $" ({revcomm.Id}, {repository.name}, {pull.Number}, {true}, '', '', ''),";
+                                        var published_comments = await installationClient.PullRequest.Review.GetAllComments(owner, repoName, pull.Number, review.Id);
+                                        var comment_id_list = string.Join(",", published_comments.Select(c => c.Id));
+
+                                        review_head_query += $" ({review.Id}, '{repoName}', {pull.Number}, '{review.Body}', '{review.State.StringValue}', ARRAY[{comment_id_list}]::bigint[]),";
+
+                                        foreach (var revcomm in published_comments)
+                                        {
+                                            comm_query += $" ({revcomm.Id}, '{repository.name}', {pull.Number}, {true}),";
+                                        }
+
                                     }
 
-                                    comm_query = comm_query[..^1];
-                                    using (var command = new NpgsqlCommand(comm_query, connection))
-                                    {
-                                        command.ExecuteNonQuery();
-                                    }
                                 }
-                                query = query.Substring(0, query.Length - 2);
+                                query = query[..^2];
                                 using (var command = new NpgsqlCommand(query, connection))
+                                {
+                                    command.ExecuteNonQuery();
+                                }
+
+                                review_head_query = review_head_query[..^1];
+                                using (var command = new NpgsqlCommand(review_head_query, connection))
+                                {
+                                    command.ExecuteNonQuery();
+                                }
+
+                                comm_query = comm_query[..^1];
+                                using (var command = new NpgsqlCommand(comm_query, connection))
                                 {
                                     command.ExecuteNonQuery();
                                 }
@@ -387,7 +398,8 @@ namespace CS.Web.Controllers
 
                             string query1 = "DELETE FROM repositoryinfo WHERE id = @id";
                             string query2 = "DELETE FROM pullrequestinfo WHERE repoid = @id";
-                            string query3 = $"DELETE FROM comments WHERE reponame = {repository.name}";
+                            string query3 = $"DELETE FROM comments WHERE reponame = '{repository.name}'";
+                            string query4 = $"DELETE FROM reviewhead WHERE reponame = '{repository.name}'";
 
 
                             // GetRepository by id lazım...
@@ -410,11 +422,16 @@ namespace CS.Web.Controllers
                                 command.ExecuteNonQuery();
                             }
 
+                            using (NpgsqlCommand command = new NpgsqlCommand(query4, connection))
+                            {
+                                command.ExecuteNonQuery();
+                            }
+
                             if (installationPayload.installation.account.type == "User")
                             {
-                                string query4 = "DELETE FROM userinfo WHERE userid = @userid";
+                                string query5 = "DELETE FROM userinfo WHERE userid = @userid";
 
-                                using (NpgsqlCommand command = new NpgsqlCommand(query4, connection))
+                                using (NpgsqlCommand command = new NpgsqlCommand(query5, connection))
                                 {
                                     command.Parameters.AddWithValue("@userid", installationPayload.installation.account.id);
 
@@ -611,7 +628,7 @@ namespace CS.Web.Controllers
                                     var installationReviewsJson = JsonConvert.SerializeObject(installationLatestReviews);                                
 
 
-                                    query += $"({repository.id}, {pull.Id}, '{pull.Base.Repository.Name}', {pull.Number}, '{pull.Title}', '{pull.User.Login}', '{pull.User.AvatarUrl}', '{pull.CreatedAt.Date:yyyy-MM-dd}', '{pull.UpdatedAt.Date:yyyy-MM-dd}', {pull.Comments}, {pull.Commits}, {pull.ChangedFiles}, {pull.Additions}, {pull.Deletions}, {pull.Draft}, {pull.Merged}, '{pull.State.ToString()}', {requestedReviewers}, '{labeljson}', '{pull.Url}', '{pull.Base.Repository.Owner.Login}', '{JsonConvert.SerializeObject(checksList)}', {checks_complete_count}, {checks_incomplete_count}, {checks_success_count}, {checks_fail_count}, {assignedReviewers}, '{installationReviewsJson}', {priority}), ";
+                                    query += $"({repository.id}, {pull.Id}, '{pull.Base.Repository.Name}', {pull.Number}, '{pull.Title}', '{pull.User.Login}', '{pull.User.AvatarUrl}', '{pull.CreatedAt.Date:yyyy-MM-dd}', '{pull.UpdatedAt.Date:yyyy-MM-dd}', {pull.Comments}, {pull.Commits}, {pull.ChangedFiles}, {pull.Additions}, {pull.Deletions}, {pull.Draft}, {pull.Merged}, '{pull.State.StringValue}', {requestedReviewers}, '{labeljson}', '{pull.Url}', '{pull.Base.Repository.Owner.Login}', '{JsonConvert.SerializeObject(checksList)}', {checks_complete_count}, {checks_incomplete_count}, {checks_success_count}, {checks_fail_count}, {assignedReviewers}, '{installationReviewsJson}', {priority}), ";
                                 
                                     var comments = await installationClient.Issue.Comment.GetAllForIssue(repository.id, repoPull.Number);
                                     foreach (var comm in comments)
@@ -656,6 +673,7 @@ namespace CS.Web.Controllers
                             string query1 = "DELETE FROM repositoryinfo WHERE id = @id";
                             string query2 = "DELETE FROM pullrequestinfo WHERE repoid = @id";
                             string query3 = $"DELETE FROM comments WHERE reponame = '{repository.name}'";
+                            string query4 = $"DELETE FROM reviewhead WHERE reponame = '{repository.name}'";
 
                             // GetRepository by id lazım...
                             using (NpgsqlCommand command = new NpgsqlCommand(query1, connection))
@@ -673,6 +691,11 @@ namespace CS.Web.Controllers
                             }
 
                             using (NpgsqlCommand command = new NpgsqlCommand(query3, connection))
+                            {
+                                command.ExecuteNonQuery();
+                            }
+
+                            using (NpgsqlCommand command = new NpgsqlCommand(query4, connection))
                             {
                                 command.ExecuteNonQuery();
                             }
