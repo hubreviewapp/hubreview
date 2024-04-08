16fb7dfa7c2e63f859438204a4af17ab2dde0a90
vIremAydin <viremaydin@gmail.com>
2024-04-08 03:41:25 +0300

    show pr review status

--- web-client/src/utility/WorkloadBarColor.ts
+++ web-client/src/utility/WorkloadBarColor.ts
@@ -1,6 +1,14 @@
 function BarColor(capacity: number, waiting: number) {
   const workload = (waiting / capacity) * 100;
-  return workload > 80 ? "red" : workload > 60 ? "orange" : workload > 40 ? "yellow" : "green";
+  return workload > 80
+    ? "red"
+    : workload > 60
+      ? "orange"
+      : workload > 40
+        ? "yellow"
+        : workload > 20
+          ? "yellowgreen"
+          : "green";
 }
 
 export default BarColor;
