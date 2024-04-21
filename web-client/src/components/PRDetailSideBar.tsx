cc6c29cc02921e1cfe2e084be353440a4db0e08e
Vesile İrem Aydın <67067747+vIremAydin@users.noreply.github.com>
2024-04-22 02:02:54 +0300

    Irem/repo config (#198)
    Co-authored-by: Alper Mumcular <alper.mumcular@ug.bilkent.edu.tr>
    Co-authored-by: Ece Kahraman <ece.kahraman@ug.bilkent.edu.tr>

--- web-client/src/components/PRDetailSideBar.tsx
+++ web-client/src/components/PRDetailSideBar.tsx
@@ -226,24 +226,26 @@ function PRDetailSideBar({ addedReviewers, labels, addedAssignees, author }: PRD
   };
 
   function changePriority(value: PriorityBadgeLabel) {
+    console.log("value", value);
+    console.log(priority);
     // delete the priority label
-    if (value == null) {
-      if (priority) {
-        axios
-          .delete(
-            `${BASE_URL}/api/github/pullrequest/${owner}/${repoName}/${prnumber}/${"Priority: ".concat(
-              priority.toString(),
-            )}`,
-            {
-              withCredentials: true,
-            },
-          )
-          .then(function () {})
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
 
+    if (priority != null) {
+      axios
+        .delete(
+          `${BASE_URL}/api/github/pullrequest/${owner}/${repoName}/${prnumber}/${"Priority: ".concat(
+            priority.toString(),
+          )}`,
+          {
+            withCredentials: true,
+          },
+        )
+        .then(function () {})
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    if (value == null) {
       setPriority(value);
       return;
     }
