981b8160c4198fbfedf8f7b4fb18ded08d1a2790
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2024-04-30 14:11:34 +0300

    Update NavBar.tsx

--- web-client/src/components/NavBar.tsx
+++ web-client/src/components/NavBar.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { IconLogout } from "@tabler/icons-react";
 import { useUser } from "../providers/context-utilities";
 import Logo from "../assets/icons/logo-color.svg";
-import SignIn from "../assets/icons/signin.png";
 
 function NavBar() {
   const location = useLocation();
@@ -41,54 +40,51 @@ function NavBar() {
 
   return (
     <Box bg="#0D1B2A" p="20px">
+      <Grid>
+        <Grid.Col span={2.5}>
+          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+            <Image h={50} src={Logo} />
+          </Link>
+        </Grid.Col>
+        <Grid.Col span={2.5}></Grid.Col>
+        <Grid.Col span={7} style={{ display: "flex", justifyContent: "space-evenly", marginTop: "10px" }}>
+          <Button
+            component={Link}
+            variant={isActive == 1 ? "outline" : "transparent"}
+            to="/"
+            onClick={() => handleClick(1)}
+          >
+            Review Queue
+          </Button>
+          <Button
+            component={Link}
+            variant={isActive == 2 ? "outline" : "transparent"}
+            to="/repositories"
+            onClick={() => handleClick(2)}
+          >
+            Repositories
+          </Button>
+          <Button
+            component={Link}
+            variant={isActive == 3 ? "outline" : "transparent"}
+            to="/analytics"
+            onClick={() => handleClick(3)}
+          >
+            Analytics
+          </Button>
+          {location.pathname !== "/signIn" && user && (
+            <Button variant="transparent" component="a" href={"https://github.com/" + user.login} target="_blank">
+              <Avatar src={user.avatarUrl} radius="xl" size="2rem" />
+              <Space w="xs" />
+              {user.login}
+            </Button>
+          )}
 
-        <Grid>
-          <Grid.Col span={2.5}>
-            <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-              <Image h={50} src={Logo} />
-            </Link>
-          </Grid.Col>
-          <Grid.Col span={2.5}>
-          </Grid.Col>
-          <Grid.Col span={7} style={{ display: "flex", justifyContent: "space-evenly", marginTop: "10px"}}>
-            <Button
-              component={Link}
-              variant={isActive == 1 ? "outline" : "transparent"}
-              to="/"
-              onClick={() => handleClick(1)}
-            >
-              Review Queue
-            </Button>
-            <Button
-              component={Link}
-              variant={isActive == 2 ? "outline" : "transparent"}
-              to="/repositories"
-              onClick={() => handleClick(2)}
-            >
-              Repositories
-            </Button>
-            <Button
-              component={Link}
-              variant={isActive == 3 ? "outline" : "transparent"}
-              to="/analytics"
-              onClick={() => handleClick(3)}
-            >
-              Analytics
-            </Button>
-            {location.pathname !== "/signIn" && user && (
-              <Button variant="transparent" component="a" href={"https://github.com/" + user.login} target="_blank">
-                <Avatar src={user.avatarUrl} radius="xl" size="2rem" />
-                <Space w="xs" />
-                {user.login}
-              </Button>
-            )}
-
-            <Button rightSection={iconLogout} variant="transparent" onClick={() => handleClick(0)}>
-              Log out
-            </Button>
-          </Grid.Col>
-        </Grid>
-
+          <Button rightSection={iconLogout} variant="transparent" onClick={() => handleClick(0)}>
+            Log out
+          </Button>
+        </Grid.Col>
+      </Grid>
     </Box>
   );
 }
